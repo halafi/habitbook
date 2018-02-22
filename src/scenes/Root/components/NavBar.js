a9628049e57d7e58b6daeb81b6098e165900c113
filip.halas <filip.halas@kiwi.com>
2018-02-22 19:05:16 +0100

    feat(navbar): menu

--- src/scenes/Root/components/NavBar.js
+++ src/scenes/Root/components/NavBar.js
@@ -9,6 +9,9 @@ import { withStyles } from 'material-ui/styles'
 import AppBar from 'material-ui/AppBar'
 import Toolbar from 'material-ui/Toolbar'
 import Typography from 'material-ui/Typography'
+import IconButton from 'material-ui/IconButton'
+import Avatar from 'material-ui/Avatar'
+import Menu, { MenuItem } from 'material-ui/Menu'
 import Button from 'material-ui/Button'
 import type { Profile } from '../../../common/records/Firebase/Profile'
 import { selectUser } from '../../../common/actions/dashboardActions'
@@ -28,7 +31,14 @@ type Props = {
   selectedUserId: string,
 }
 
-class NavBar extends React.Component<Props> {
+type State = {
+  anchorEl: any,
+}
+
+class NavBar extends React.Component<Props, State> {
+  state = {
+    anchorEl: null,
+  }
   handleReload = () => {
     const { selectUserAction, selectedUserId } = this.props
 
@@ -37,40 +47,54 @@ class NavBar extends React.Component<Props> {
     }
   }
 
+  handleMenu = event => this.setState({ anchorEl: event.currentTarget })
+  handleClose = () => this.setState({ anchorEl: null })
+
   render() {
     const { classes, firebase, profile } = this.props
+    const { anchorEl } = this.state
+
+    const menuOpen = Boolean(anchorEl)
+    const loggedIn = isLoaded(profile) && !isEmpty(profile)
+    const unauthenticated = isLoaded(profile) && isEmpty(profile)
 
     return (
       <AppBar position="static" color="default">
         <Toolbar>
-          <Typography type="title" className={classes.flex}>
+          <Typography variant="title" className={classes.flex}>
             <Button onClick={this.handleReload}>Habitbook</Button>
           </Typography>
           <div>
-            {isLoaded(profile) &&
-              isEmpty(profile) && (
-                <Button
-                  onClick={() => {
-                    firebase.login({
-                      provider: 'google',
-                      type: 'redirect',
-                    })
-                  }}
-                >
-                  Login
-                </Button>
-              )}
-            {isLoaded(profile) &&
-              !isEmpty(profile) && (
-                <Button
-                  onClick={() => {
-                    firebase.logout()
-                  }}
-                >
-                  Logout ({profile.displayName})
-                </Button>
-              )}
+            {unauthenticated && (
+              <Button
+                onClick={() => {
+                  firebase.login({
+                    provider: 'google',
+                    type: 'redirect',
+                  })
+                }}
+              >
+                Login
+              </Button>
+            )}
           </div>
+          {loggedIn && (
+            <div>
+              <IconButton
+                aria-owns={menuOpen ? 'menu-appbar' : null}
+                aria-haspopup="true"
+                onClick={this.handleMenu}
+                color="inherit"
+              >
+                <Avatar src={profile.avatarUrl} />
+              </IconButton>
+              <Menu id="menu-appbar" open={menuOpen} onClose={this.handleClose} anchorEl={anchorEl}>
+                <MenuItem onClick={this.handleClose}>Profile</MenuItem>
+                <MenuItem disabled onClick={this.handleClose}>My account</MenuItem>
+                <MenuItem disabled onClick={() => firebase.logout()}>Logout</MenuItem>
+              </Menu>
+            </div>
+          )}
         </Toolbar>
       </AppBar>
     )
